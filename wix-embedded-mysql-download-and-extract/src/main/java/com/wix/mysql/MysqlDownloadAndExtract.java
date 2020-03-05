7f7bfa876968d5450334e265f6f0ddfc0cb35426
Amrith Nayak <shroff.amrith@flipkart.com>
2020-03-05 21:17:33 +0530

    - Added support to specify an alternate base URL for downloading a copy of MySql for hermetic environments

--- wix-embedded-mysql-download-and-extract/src/main/java/com/wix/mysql/MysqlDownloadAndExtract.java
+++ wix-embedded-mysql-download-and-extract/src/main/java/com/wix/mysql/MysqlDownloadAndExtract.java
@@ -11,11 +11,19 @@ import static com.wix.mysql.config.DownloadConfig.aDownloadConfig;
 public class MysqlDownloadAndExtract {
 
     public static void main(String[] args) {
-        DownloadConfig downloadConfig = aDownloadConfig().withCacheDir(args[0]).build();
+        final DownloadConfig.Builder downloadConfigBuilder = aDownloadConfig().withCacheDir(args[0]);
+        perhapsSetBaseUrl(args, downloadConfigBuilder);
+        DownloadConfig downloadConfig = downloadConfigBuilder.build();
         MysqldConfig mysqldConfig = MysqldConfig.aMysqldConfig(Version.valueOf(version(args))).build();
         IRuntimeConfig runtimeConfig = new RuntimeConfigBuilder().defaults(mysqldConfig, downloadConfig).build();
         MysqldStarter mysqldStarter = new MysqldStarter(runtimeConfig);
         mysqldStarter.prepare(mysqldConfig);
+    }
+
+    private static void perhapsSetBaseUrl(final String[] args, final DownloadConfig.Builder downloadConfigBuilder) {
+        if (args.length > 3) {
+            downloadConfigBuilder.withBaseUrl(args[3]);
+        }
     }
 
     private static String version(final String[] args) {
